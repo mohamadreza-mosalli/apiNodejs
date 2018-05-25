a1f95a903d8edb3aa5b4813b487ed645badefc5b
Mohamadrzea Mosalli <mohamadrezamosalli@gmail.com>
2018-05-25 07:18:03 +0430

    generat token

--- app/controllers/auth/registerController.js
+++ app/controllers/auth/registerController.js
@@ -1,20 +1,36 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../../models/user');
+
+
+signToken = user => {
+    return jwt.sign({ 
+        iss : 'Hexzm' ,
+        sub : user.id,
+        iat : new Date().getTime(),// current time
+        exp : new Date().setDate(new Date().getDate() + 3)// current time + 3 day
+    } , process.env.SECRET_KEY);
+}
+
 module.exports = {
 
     proccess : async (req , res , next) => {
         const { email , password } = req.value.body;
 
-        //check if there is a user with the same email
+        // check if there is a user with the same email
         const foundUser = await User.findOne({ email });
         if(foundUser){
             return res.status(403).json({error : 'The user has already registered with this email'});
         }
         
-        //create user
+        // create user
         const newUser = new User({ email , password });
-        await newUser.save( err => {
-            if(err) console.log(err);
-            res.status(200).json('create User');
+        await newUser.save( err =>{ 
+            if(err) console.log(err)
+            else{
+                //generat token
+                const token = signToken(newUser)
+                res.status(200).json({ token });
+            }
         });
     }
 
